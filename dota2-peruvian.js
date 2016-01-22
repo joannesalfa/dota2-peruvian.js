30bf07252cdf41d159b944d2d78a05db462d3b1a
Ivan <joannesalfa@gmail.com>
2016-01-21 22:35:02 -0600

    first blood

---
+++ dota2-peruvian.js
@@ -0,0 +1,24 @@
+//Traits of a peruvian
+var peruvianFace = {
+  1:"Inca hair",
+  2:"Bird nose",
+  3:"Small eyes",
+  4:"Upper cheeks",
+  5:"Injun mouth"
+};
+
+//Pick random peruvian trait
+function peruvianFaceRandom(){
+  var randomize = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+  return peruvianFace[randomize];
+}
+
+//Call number of peruvian trait
+function peruvianFaceNum(num){
+  if (peruvian.hasOwnProperty(num) === true){
+   return peruvianFace[num]
+  }
+  else {
+   return "agg tmr gg";
+  }
+};
